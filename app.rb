2b8c039872a625c512e54f7bc79dc53646184dbc
MarySalemme <marysalemme1@gmail.com>
2017-06-19 16:03:39 +0100

    Adding image

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
- get '/' do
-   'Hello!'
- end
+require 'shotgun'
 
- get '/secret' do
-   'This is probably a secret message'
- end
+get '/' do
+  'Hello world!'
+end
+
+get '/secret' do
+  'Secret message'
+end
+
+get '/cat' do
+  '<img style="border:5px dotted red;" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png" alt="Kittie">'
+end
