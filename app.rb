7d971e77451cd058af0169c555405c225888bffb
MarySalemme <marysalemme1@gmail.com>
2017-06-19 16:19:56 +0100

    erb file

--- app.rb
+++ app.rb
@@ -10,5 +10,5 @@ get '/secret' do
 end
 
 get '/cat' do
-  '<img style="border:5px dotted red;" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png" alt="Kittie">'
+  erb(:index)
 end
